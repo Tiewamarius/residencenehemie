032d560d18f40eae4a5904d01c8afcf1ec34e38a
Tiewamarius <yobouetiewamaruis@gmail.com>
2025-07-24 18:00:18 +0000

    Mon commit sur la branche master

--- resources/views/Pages/HomePage.blade.php
+++ resources/views/Pages/HomePage.blade.php
@@ -132,13 +132,6 @@
                                 <option value="1">1</option>
                                 <option value="2" selected>2</option>
                                 <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
                             </select>
                         </div>
                         <div class="form-group select-input">
@@ -148,13 +141,7 @@
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
+                                <option value="3">4</option>
                             </select>
                         </div>
                     </div>

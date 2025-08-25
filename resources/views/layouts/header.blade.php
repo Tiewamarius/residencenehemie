6f2f2dfad844b0460b1599ac18781988e6856921
Tiewamarius <tiewamaruis@gmail.com>
2025-08-25 08:04:44 +0000

    icon login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,6 +11,7 @@
             <!-- <li><a href="#" class="nav_link" id="search-toggle-btn-desktop"><i class="fas fa-search"></i></a></li> -->
 
             @guest
+            <li><a href="{{ route('login') }}" class="sidebar_nav_link"><i class="fas fa-user-circle"></i></a></li>
             <!-- <li><a href="#" class="nav_link" id="open-login-modal"><i class="fas fa-heart"></i></a></li> -->
             @else
             <!-- <li><a href="{{ route('favorites.index') }}" class="nav_link"><i class="fas fa-heart"></i></a></li> -->
